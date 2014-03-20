5f42ea1cdcd1095aea0107c363df549730cb864e
Iván Sevillano García <isega24@hotmail.com>
2014-03-20 18:59:28 +0100

    Create testing.py

---
+++ testing.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+from baraja import *
+from mano import *
+from crupier import *
+from variables import *
+from impresor import *
+from juego import *
+from clasificador import *
+
+
+juego = Juego()
+
+ganador = juego.juega()
